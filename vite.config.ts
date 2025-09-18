ad0419da5f19252bd18c42ebe72dfbf1b6a2d150
Jason <taylorjason@users.noreply.github.com>
2025-09-18 01:01:16 -0500

    Update vite.config.ts
    remove basename

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Templater/',
+  // base: '/Templater/',
   build: {
     outDir: "build" // <- make sure this is set to "build"
   }
