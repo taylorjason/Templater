550e0353b80219e741420575e6517bf6417d741d
Jason <taylorjason@users.noreply.github.com>
2025-09-18 00:42:04 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import "./index.css"; // optional, your styles
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root!).render(
-  <BrowserRouter basename="/templater">
+  <BrowserRouter basename="/Templater">
     <App />
   </BrowserRouter>
 );
